000fc7cec0cbf3eb498ed3283f38d49c0a53f057
FahimFBA <fahimbinamin@gmail.com>
2022-07-15 08:30:51 +0600

    Update solve1.cpp

--- Prime Number/solve1.cpp
+++ Prime Number/solve1.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 bool isPrime (int n)
 {
+    if (n <= 1)
+    {
+        return false;
+    }
     for (int i = 2; i < n; ++i) {
         if (n % i == 0)
         {
@@ -15,5 +19,11 @@ bool isPrime (int n)
 int main()
 {
     cout << isPrime(9) << endl;
+    cout << isPrime(100) << endl;
+    cout << isPrime(7) << endl;
+    cout << isPrime(2) << endl;
+    cout << isPrime(1) << endl;
+    // 1 for true
+    // 0 for false
     return 0;
 }
