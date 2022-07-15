ad98165747ee37d56e41f7eec79c3966750742fe
FahimFBA <fahimbinamin@gmail.com>
2022-07-15 08:40:19 +0600

    Update solve1.cpp

--- Prime Number/solve1.cpp
+++ Prime Number/solve1.cpp
@@ -4,14 +4,11 @@ using namespace std;
 bool isPrime (int n)
 {
     if (n <= 1)
-    {
         return false;
-    }
+    
     for (int i = 2; i < n; ++i) {
         if (n % i == 0)
-        {
-            return false;
-        }
+            return false;        
     }
     return true;
 }
